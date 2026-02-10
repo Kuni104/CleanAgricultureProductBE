1aa108bcccceae4d829b014598fd5a41b2dfc838
Kuni <binlen11112@gmail.com>
2026-02-10 13:01:16 +0700

    change CartItem model, refactor cartService

--- CleanAgricultureProductBE/Repositories/Cart/ICartRepository.cs
+++ CleanAgricultureProductBE/Repositories/Cart/ICartRepository.cs
@@ -4,6 +4,19 @@ namespace CleanAgricultureProductBE.Repositories.Cart
 {
     public interface ICartRepository
     {
+        //Cart
         public Task<Models.Cart?> GetCartByCustomerId(Guid customerId);
+        //Cart Items
+        public Task AddCartItem(Models.CartItem cartItem);
+        public Task<List<Models.CartItem>> GetCartItemsByCartId(Guid cartId);
+        public Task<List<Models.CartItem>> GetCartItemsByCartIdIncludeProduct(Guid cartId);
+        public Task<List<Models.CartItem>> GetCartItemsByCartIdWithPagination(Guid cartId, int offset, int pageSize);
+        public Task<bool> CheckCartItemByCartIdAndProductId(Guid cartId, Guid productId);
+        public Task<Models.CartItem?> GetCartItemByCartIdAndProductId(Guid cartId, Guid productId);
+        public Task<Models.CartItem?> GetCartItemByCartIdAndProductIdIncludeProduct(Guid cartId, Guid productId);
+        public Task UpdateCartItem(Models.CartItem cartItem);
+        public Task DeleteCartItem(Models.CartItem cartItem);
+        public Task DeleteAllCartItems(Guid cartId);
+        public Task<decimal> TotalPriceOfCartByCartId(Guid cartId);
     }
 }
--- CleanAgricultureProductBE/Repositories/CartItem/ICartItemRepository.cs
+++ CleanAgricultureProductBE/Repositories/CartItem/ICartItemRepository.cs
@@ -3,7 +3,6 @@
     public interface ICartItemRepository
     {
         public Task AddCartItem(Models.CartItem cartItem);
-
         public Task<List<Models.CartItem>> GetCartItemsByCartId(Guid cartId);
         public Task<List<Models.CartItem>> GetCartItemsByCartIdWithPagination(Guid cartId, int offset, int pageSize);
         public Task<bool> CheckCartItemByCartIdAndProductId(Guid cartId, Guid productId);
