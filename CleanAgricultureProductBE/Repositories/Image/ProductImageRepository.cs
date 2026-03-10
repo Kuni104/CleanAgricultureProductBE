f58217affa3a1af6d83aa9745dc25fcf2a64ae09
Tunna-github <trungvungoc2906@gmail.com>
2026-03-10 15:48:06 +0700

    Add UploadProductImage

---
+++ CleanAgricultureProductBE/Repositories/Image/ProductImageRepository.cs
@@ -0,0 +1,20 @@
+﻿using CleanAgricultureProductBE.Data;
+using CleanAgricultureProductBE.Models;
+
+namespace CleanAgricultureProductBE.Repositories.Image
+{
+    public class ProductImageRepository : IProductImageRepository
+    {
+        private readonly AppDbContext _context;
+        public ProductImageRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddImagesAsync(List<ProductImage> images)
+        {
+            await _context.ProductImages.AddRangeAsync(images);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ CleanAgricultureProductBE/Services/Image/IProductImageService.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanAgricultureProductBE.Services.Image
+{
+    public interface IProductImageService
+    {
+        Task<List<string>> UploadProductImagesAsync(Guid productId, List<IFormFile> imagegs);
+    }
+}
